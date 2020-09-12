607217961696d00d584cde0f157651899fd75d7d
Ellen Giacometti <39375569+ellengiacometti@users.noreply.github.com>
2020-09-12 12:28:31 -0300

    Add APIs Watson e processamento de audio

--- voice_recognition.py
+++ voice_recognition.py
@@ -1,57 +1,32 @@
-import sounddevice as sd
-import wavio
-import os
-import numpy as np
-import scipy
+
 from playsound import playsound
-from fuzzywuzzy import fuzz
-from watson_developer_cloud import SpeechToTextV1
+import interface_ibm as ibm
+import manipula_audio as ma
+import time
 
 
 
-#TODO: Criptografar e disponibilizar em arquivo de configuração para fácil alteração
-speech_to_text = SpeechToTextV1(
-    iam_apikey='85dIoj9uglDt40vrSGysIwl1sH1llA9yVvQkEMqG4xTb',
-    url='https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/98493ae6-0de0-4eab-8841-4a9ac54b1ab7'
-)
 
-# Criando variáveis de uso recorrente
-login="Ellen"
-audio_filename="output.wav"
-dir=" "
-usuario_dir=""
-palavras_teste =["Vou","ser","um","technee","Safra"] # palavras para testar  a precisão do modelo
-fs = 44100  # Taxa de amostragem
-segundos = 4  # Duração em segundos do audio
-print("[AUDIO_INPUT] Gravando audio")
-audio = sd.rec(int(segundos * fs), samplerate=fs, channels=2)
-sd.wait()  # Aguarda enquanto o audio está sendo gravado
-print("[AUDIO_INPUT] Salvando audio no formato .wav")
-wavio.write(audio_filename, audio,fs,sampwidth=2)
-print("[AUDIO_INPUT] Tocando audio")
-playsound(audio_filename)
-
-# Rotina de integração com a IBM
-with open(audio_filename.wav, 'rb') as audio_file:
-    palavras_reconhecidas = speech_to_text.recognize(audio_file, content_type='audio/wav').get_result()
-palavras_reconhecidas = str(palavras_reconhecidas['results'][0]['alternatives'][0]['transcript'])
-
-print("[WATSON] Transcrição Watson : " + palavras_reconhecidas)
-
-# Métricas para avaliação de performance
-print("[FUZZY] Fuzzy Score Parcial: " + str(fuzz.partial_ratio(palavras_teste, palavras_reconhecidas)))
-print("[FUZZY] Fuzzy Score : " + str(fuzz.ratio(palavras_teste, palavras_reconhecidas)))
+if __name__ == "__main__":
 
 
-features = np.asarray(())# Alocando array para os atributos do modelo
-for file in os.listdir(dir):#Varrendo o diretórios procurando por .wav para extrair features
-    filename_wav = os.fsdecode(file)
-    if filename_wav.endswith(".wav"):
-              print("[FEATURE EXTRACTION] : Reading audio files for processing ...")
-                (taxa, sinal) = scipy.io.wavfile.read(usuario_dir + filename_wav)
-              #TODO: Criar função para extração de features
-              #TODO: Criar função filtro para atenuar ruídos
-              #TODO: Definir modelo e treiná-lo com base nos audios adquiridos no momento da inscrição
+
+    ## Gravar Audios
+    nome_audio_entrada=["Intenção01.wav","Intenção02.wav","Intenção03.wav","Intenção04.wav","Intenção05.wav","Intenção06.wav","Intenção07.wav"]
+    # gravarAudio(nome_audios)
+    ## Teste
+    audio_crip=ma.encodeAudio(nome_audio_entrada[0])
+    ma.decodeAudio(audio_crip,"TesteCrip.wav")
+    # # tipo_modelo ='pt-BR_NarrowbandModel' /'pt-BR_BroadbandModel'
+    transcript=ibm.voz2Texto("TesteCrip.wav")
+
+    print("Transcript:",transcript)
+    [intencao,confianca]=ibm.texto2Intencao(transcript)
+    print("Intenção:",intencao)
+    print("Confiança:",confianca)
+
+    # voz_modelo= pt-BR_IsabelaVoice,pt-BR_IsabelaV3Voice,
+
 
 
 
